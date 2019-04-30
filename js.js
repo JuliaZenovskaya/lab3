f104c299388ced9d83eb8be49563a2d6fc586745
JuliaZenovskaya <juliazenovskaya@gmail.com>
2019-05-01 00:18:05 +0300

    j

--- js.js
+++ js.js
@@ -8,8 +8,8 @@ function createPage(){
     document.body.appendChild(myDiv);
     
     canvas = document.createElement("canvas");
-    canvas.width = myDiv.width;
-    canvas.height = myDiv.height;
+    canvas.width = 600;
+    canvas.height = 400;
     myDiv.appendChild(canvas);
     
     drawImages();
@@ -20,26 +20,27 @@ function createPage(){
 function drawImages() {
     var context = canvas.getContext("2d");
 
-    var x = Math.floor(Math.random() * (canvas.width/3)) + canvas.width/3;
-    var y = Math.floor(Math.random() * (canvas.height/3)) + canvas.height/3;
+    //var x = Math.floor(Math.random() * (canvas.width/3)) + canvas.width/3;
+    //var y = Math.floor(Math.random() * (canvas.height/3)) + canvas.height/3;
+    var xForWidth = 300;
+    var yForHeight = 200;
     
     var countDrawnImages = 0;
     var image;
-    //for (var i = 0; i < 4; i++){
-    var i = 0;
+    for (var i = 0; i < 4; i++){
         image = new Image();
         image.crossOrigin = "Anonymous";
         
-        var xForWidth = x;
+        //var xForWidth = x;
         var xForBeginning = 0;
         if (i % 2 == 1){
-            xForWidth = canvas.width - x;
+            //xForWidth = canvas.width - x;
             xForBeginning = x;
         }
-        var yForHeight = y;
+        //var yForHeight = y;
         var yForBeginning = 0;
         if (i > 1){
-            yForHeight = canvas.height - y;
+            //yForHeight = canvas.height - y;
             yForBeginning = y;
         }
         image.src = 'https://source.unsplash.com/' + xForWidth + 'x' + yForHeight;
