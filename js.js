3d68515a36a91595631d9a7847efc3b5ce934551
JuliaZenovskaya <juliazenovskaya@gmail.com>
2019-04-30 23:26:13 +0300

    j

--- js.js
+++ js.js
@@ -24,33 +24,38 @@ function drawImages() {
     var y = Math.floor(Math.random() * (canvas.height/3)) + canvas.height/3;
     
     var countDrawnImages = 0;
-    var image;
-    for (var i = 0; i < 1; i++){
-        image = new Image();
-        image.crossOrigin = "Anonymous";
-        
-        var xForWidth = x;
-        var xForBeginning = 0;
-        if (i % 2 == 1){
-            xForWidth = canvas.width - x;
-            xForBeginning = x;
-        }
-        var yForHeight = y;
-        var yForBeginning = 0;
-        if (i > 1){
-            yForHeight = canvas.height - y;
-            yForBeginning = y;
-        }
-        image.src = 'https://source.unsplash.com/' + xForWidth + 'x' + yForHeight;
-        
-        image.onload = function() {
-            context.drawImage(image, xForBeginning, yForBeginning);
-            countDrawnImages += 1;         
-            if (countDrawnImages == 4) {
-                drawText();
-            }
-        }
+    var image = new Image();
+    image.crossOrigin = "Anonymous";
+    image.src = 'https://source.unsplash.com/' + canvas.width + 'x' + canvas.height;
+    image.onload = function(){
+        context.drawImage(image, 0,0);
     }
+    //for (var i = 0; i < 4; i++){
+    //    image = new Image();
+    //    image.crossOrigin = "Anonymous";
+    //    
+    //    var xForWidth = x;
+    //    var xForBeginning = 0;
+    //    if (i % 2 == 1){
+    //        xForWidth = canvas.width - x;
+    //        xForBeginning = x;
+    //    }
+    //    var yForHeight = y;
+    //    var yForBeginning = 0;
+    //    if (i > 1){
+    //        yForHeight = canvas.height - y;
+    //        yForBeginning = y;
+    //    }
+    //    image.src = 'https://source.unsplash.com/' + xForWidth + 'x' + yForHeight;
+    //    
+    //    image.onload = function() {
+    //        context.drawImage(image, xForBeginning, yForBeginning);
+    //        countDrawnImages += 1;         
+    //        if (countDrawnImages == 4) {
+    //            drawText();
+    //        }
+    //    }
+    //}
 }
 
 function drawText() {
